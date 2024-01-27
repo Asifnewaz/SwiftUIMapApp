b52db2ee7efea0063a846779733f32282420be40
AsifNewaz <asifnewaz.cste@gmail.com>
2024-01-27 22:24:22 +0100

    Custom Map Annotation Pins for SwiftUI MapKit Map | SwiftUI Map App

---
+++ SwiftUIMapApp/SwiftUIMapApp/Views/LocatioMapAnnotationView.swift
@@ -0,0 +1,39 @@
+//
+//  LocatioMapAnnotationView.swift
+//  SwiftUIMapApp
+//
+//  Created by USER on 1/27/24.
+//
+
+import SwiftUI
+
+struct LocatioMapAnnotationView: View {
+    let accentColor = Color("AccentColor")
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            Image(systemName: "map.circle.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 30, height: 30)
+                .font(.headline)
+                .foregroundColor(Color.white)
+                .padding(6)
+                .background(accentColor)
+                .cornerRadius(36)
+            
+            Image(systemName: "triangle.fill")
+                .resizable()
+                .scaledToFit()
+                .foregroundColor(accentColor)
+                .frame(width: 10, height: 10)
+                .rotationEffect(Angle(degrees: 180))
+                .offset(y: -3)
+                .padding(.bottom, 40)
+        }
+    }
+}
+
+#Preview {
+    LocatioMapAnnotationView()
+}
